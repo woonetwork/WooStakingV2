11712632e6595421b5460b8b396d7815c1f1532f
luke <yacphilo@gmail.com>
2023-03-11 04:13:20 -0500

    feat: Update deploy contracts

---
+++ scripts/constants.ts
@@ -0,0 +1,15 @@
+export const depAddressList = {
+  "woo": "0x6626c47c00F1D87902fc13EECfaC3ed06D5E8D8a",
+  "wooPP": "0x286ab107c5E9083dBed35A2B5fb0242538F4f9bf",
+  "usdc": "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75",
+  "weth": "0x74b23882a30290451A17c44f4F05243b6b58C76d",
+  "lz_fantom_endpoint": "0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7"
+};
+
+export const lz_fantom_chainid = 112;
+export const user1 = "0xA113d3B08df49D442fA1c0b47A82Ad95aD19c0Fb";
+export const user2 = "0xea02DCC6fe3eC1F2a433fF8718677556a3bb3618";
+export const user3 = "0x7C8A5d20b22Ce9b369C043A3E0091b5575B732d9";
+
+export const stakingContractsFile = "./staking_contracts.json";
+export const sleepSeconds = 10000; // 10 seconds
---
+++ scripts/deploy_staking_contracts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. deploy and verify contracts
+# 2. setup contracts and set admin for contracts
+
+# NOTE: need run this script on folder WooStakingV2.
+
+npx hardhat run --network fantom_mainnet scripts/deploy_contracts.ts
+npx hardhat run --network fantom_mainnet scripts/setup_contracts.ts
+
+
