e70b27275d10d79005cad9d549ae1b40e1bebda4
0xmer1in <39942523+0xmer1in@users.noreply.github.com>
2024-06-17 22:20:46 +0800

    chore: format RewardNFT

--- contracts/RewardNFT.sol
+++ contracts/RewardNFT.sol
@@ -1,26 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
 import {BaseAdminOperation} from "./BaseAdminOperation.sol";
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract RewardNFT is ERC1155, BaseAdminOperation {
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    uint256 public constant COMMON = 1;
+    uint256 public constant UNCOMMON = 1;
     uint256 public constant RARE = 2;
     uint256 public constant EPIC = 3;
+
     uint256[] public nftTypes;
-    mapping(uint256 => bool) burnableNFT;
+    mapping(uint256 => bool) public burnables;
+
     address public campaignManager;
 
-    string private initUri = "https://oss.woo.org/static/images/nft/{id}.gif";
+    string private initUri = "https://oss.woo.org/static/images/nft/{id}.webm";
 
-    constructor() public ERC1155(initUri) {
-        _addNFTType(EPIC, true);
+    constructor() ERC1155(initUri) {
+        _addNFTType(UNCOMMON, true);
         _addNFTType(RARE, true);
-        _addNFTType(COMMON, true);
+        _addNFTType(EPIC, true);
+    }
+
+    // --------------------- Business Functions --------------------- //
+
+    function mint(address _user, uint256 _nftType, uint256 _amount) external {
+        require(msg.sender == campaignManager, "RewardNFT: !campaignManager");
+        _mint(_user, _nftType, _amount, "");
+    }
+
+    function getAllNFTTypes() external view returns (uint256[] memory) {
+        return nftTypes;
     }
 
     function _addNFTType(uint256 _nftType, bool _burnable) internal {
@@ -31,23 +45,12 @@ contract RewardNFT is ERC1155, BaseAdminOperation {
                 break;
             }
         }
-        require(exist == false, "RewardNFT: !nftType");
+        require(exist == false, "RewardNFT: !_nftType");
         nftTypes.push(_nftType);
-        burnableNFT[_nftType] = _burnable;
+        burnables[_nftType] = _burnable;
     }
 
-    function burnable(uint256 _nftType) external view returns (bool) {
-        return burnableNFT[_nftType];
-    }
-
-    function getNftTypes() external view returns (uint256[] memory) {
-        return nftTypes;
-    }
-
-    function mint(address _user, uint256 _nftType, uint256 _amount) external {
-        require(msg.sender == campaignManager, "RewardNFT: !campaignManager");
-        _mint(_user, _nftType, _amount, "");
-    }
+    // --------------------- Admin Functions --------------------- //
 
     function batchAirdrop(address[] memory _users, uint256 _nftType, uint256 _amount) external onlyAdmin {
         uint256 len = _users.length;
@@ -65,7 +68,7 @@ contract RewardNFT is ERC1155, BaseAdminOperation {
     }
 
     function setBurnable(uint256 _nftType, bool _burnable) external onlyOwner {
-        burnableNFT[_nftType] = _burnable;
+        burnables[_nftType] = _burnable;
     }
 
     function setCampaignManager(address _campaignManager) external onlyOwner {
