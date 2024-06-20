69ab89f1ea9159b7d5691100e8889f4e84039b1c
Merlin <39942523+0xmer1in@users.noreply.github.com>
2024-06-20 14:50:30 +0800

    chore: add isClaimable (#60)

--- contracts/interfaces/IRewardCampaignManager.sol
+++ contracts/interfaces/IRewardCampaignManager.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 interface IRewardCampaignManager {
     function claim(uint256 _campaignId) external returns (uint128 count);
 
-    function isClaimed(uint256 _campaignId, uint256 _nftType, address _user) external view returns (bool isClaimed);
+    function isClaimable(uint256 _campaignId, uint256 _tokenId, address _user) external view returns (bool claimable);
+
+    function isClaimed(uint256 _campaignId, uint256 _tokenId, address _user) external view returns (bool claimed);
 }
